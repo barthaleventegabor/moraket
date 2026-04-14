dcc9debc06d7d0d92f36077bb7b35afa50caa0cb
barthaleventegabor <b.levi.levi@gmail.com>
2026-04-14 10:57:43 +0200

    kész

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -2,10 +2,15 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class MainController {
+
+    int n = 90;
+
+    CheckBox[] boxes = new CheckBox[n];
 
     @FXML
     private TextField countNums;
@@ -13,9 +18,49 @@ public class MainController {
     @FXML
     private GridPane gridPane;
 
+    @FXML 
+    void initialize() {
+        for (int i = 0; i < n; i++) {
+            boxes[i] = new CheckBox();
+            boxes[i].setText(String.valueOf(i + 1));
+            boxes[i].selectedProperty().addListener((observable, oldvalue, newvalue)->{
+                this.countNums.setText(String.valueOf(countChecked()));
+            });
+            gridPane.add(boxes[i], i % 7, i / 7);
+        }
+    }
+
     @FXML
     void onClickSaveButton(ActionEvent event) {
+        this.startSave();
+    }
 
+    void startSave() {
+        if (countChecked() == 5) {
+            Storage.writeContent(generateLine());
+        }else {
+            System.out.println("Nem 5 szám van kiválasztva!");
+        }
+    }
+    int countChecked() {
+        int count = 0;
+        for (CheckBox box : boxes) {
+            if (box.isSelected()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    String generateLine(){
+        StringBuilder line = new StringBuilder();
+        for (CheckBox box : boxes) {
+            if (box.isSelected()) {
+                line.append(box.getText()).append(",");
+            }
+        }
+
+        return line.toString();
     }
 
 }
---
+++ src/main/java/com/example/Storage.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Storage {
+    public static void writeContent(String line){
+        try {
+            tryWriteContent(line);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void tryWriteContent(String line) throws IOException{
+        FileWriter fileWriter = new FileWriter("adat.txt", true);
+        fileWriter.write(line + "\n");
+        fileWriter.close();
+    }
+}
